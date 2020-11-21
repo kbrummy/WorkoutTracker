b0ce832a5c7b5a1fccf6de4958e390cdfdd4108a
Kristin Brumfield <kristinbrumfield@Kbrummy.local>
2020-11-20 20:21:49 -0800

    another commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Setting up Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// db mongo
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+// Creating Routes
+require("./routes/apiRoutes");
+require("./routes/htmlRoutes");
+
+// Starts the server to begin listening
+app.listen(PORT, () => {
+  console.log(`App listening on Port ${PORT}!`);
+});
